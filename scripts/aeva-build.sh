fe89894a025abf147214cfb46c50a21d9c83cbd1
Sandesh J. Ghimire <sandesh@aeva.ai>
2018-02-20 14:58:46 -0800

    Update build script

--- scripts/aeva-build.sh
+++ scripts/aeva-build.sh
@@ -1,7 +1,13 @@
 #!/bin/sh 
-
+#make distclean
+#source /opt/petalinux/2017.4/environment-setup-aarch64-xilinx-linux
+#source /opt/petalinux/2017.4/environment-setup-cortexa9hf-neon-xilinx-linux-gnueabi
+export ARM=arm64
+export CCOMPILER=aarch64-linux-gnu-
+export CROSS_COMPILE=aarch64-linux-gnu-
 make ARCH=arm64 xilinx_zynqmp_defconfig
 make ARCH=arm64 menuconfig
-make ARCH=arm64
+make ARCH=arm64 -j64
+make ARCH=arm64 dtbs
 
 
